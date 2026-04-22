85b4ef012d6bede9fa167825f1bb3372f521733e
Lyuuwu <kevin42572445@gmail.com>
2026-04-22 20:11:21 +0800

    detect wrong name

--- agents/__init__.py
+++ agents/__init__.py
@@ -3,7 +3,11 @@ import importlib
 from shared.base import AgentBase
 
 def build_agent(name: str, obs_dim, act_dim, max_act, config, device='cuda') -> AgentBase:
-    module = importlib.import_module(f'agents.{name}')
+    try:
+        module = importlib.import_module(f'agents.{name}')
+    except:
+        raise ValueError(f'Agent: "{name}" does not exist')
+    
     agent = module.build(
         obs_dim = obs_dim,
         act_dim = act_dim,
