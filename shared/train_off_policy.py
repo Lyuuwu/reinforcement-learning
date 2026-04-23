64e70bc8fc8a6eaf1940a2528b89c93e642c0a2c
Lyuuwu <kevin42572445@gmail.com>
2026-04-23 15:56:05 +0800

    test

--- shared/train_off_policy.py
+++ shared/train_off_policy.py
@@ -1,3 +1,5 @@
+import tracemalloc, gc
+
 import numpy as np
 import torch
 
@@ -14,6 +16,8 @@ class OffPolicyTrainer(TrainerBase):
         self.updates_per_step = updates_per_step
 
     def _main_loop(self) -> None:
+        tracemalloc.start()
+        
         # obs, _ = self.vec_env.reset(seed=self.config.seed)
         obs = self._prefill()
         cfg = self.config
@@ -51,6 +55,15 @@ class OffPolicyTrainer(TrainerBase):
 
             if self.global_env_step % cfg.save_interval < self.num_envs:
                 self._save_checkpoint(tag='latest', include_buffer=False)
+                
+            if self.global_env_step % 10_000 == 0:
+                print('=' * 60)
+                print('snapshot')
+                snapshot = tracemalloc.take_snapshot()
+                top = snapshot.statistics('lineno')[:5]
+                for stat in top:
+                    print(stat)
+                gc.collect()
         
         # final eval
         self.logger.log_print(self.evaluate(),
